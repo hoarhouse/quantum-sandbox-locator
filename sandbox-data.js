955b82c3f9fa58687b5315f417375e3d2e655214
hoarhouse <previewcapital@gmail.com>
2025-07-08 11:53:06 -0600

    Update sandbox-data.js

--- sandbox-data.js
+++ sandbox-data.js
@@ -1,47 +1,182 @@
 const sandboxData = [
   {
-    name: "Fermilab Quantum Network Testbed",
+    name: "Q‑NEXT",
+    institution: "Argonne National Laboratory",
+    focus: "Quantum Materials & Networking Foundry",
+    funding: "DOE",
+    lat: 41.7056,
+    lng: -87.6132,
+    link: "https://q-next.org/"
+  },
+  {
+    name: "C²QA (Co‑design Center for Quantum Advantage)",
+    institution: "Brookhaven National Laboratory",
+    focus: "Quantum Error Correction & Co‑design",
+    funding: "DOE",
+    lat: 40.8730,
+    lng: -72.8730,
+    link: "https://www.bnl.gov/quantumcenter/"
+  },
+  {
+    name: "SQMS Center",
     institution: "Fermilab",
+    focus: "Superconducting Quantum Systems & Sensing",
+    funding: "DOE",
+    lat: 41.8403,
+    lng: -88.2416,
+    link: "https://sqmscenter.fnal.gov/"
+  },
+  {
+    name: "QSA (Quantum Systems Accelerator)",
+    institution: "Lawrence Berkeley National Laboratory",
+    focus: "Quantum Algorithms & Device Co‑design",
+    funding: "DOE",
+    lat: 37.8753,
+    lng: -122.2570,
+    link: "https://qsa.org/"
+  },
+  {
+    name: "QSC (Quantum Science Center)",
+    institution: "Oak Ridge National Laboratory",
+    focus: "Topological Quantum Materials & Sensing",
+    funding: "DOE",
+    lat: 36.0150,
+    lng: -84.2696,
+    link: "https://qsc.ornl.gov/"
+  },
+  {
+    name: "Advanced Quantum Testbed (AQT)",
+    institution: "LBNL",
+    focus: "Superconducting QC",
+    funding: "DOE",
+    lat: 37.8753,
+    lng: -122.2570,
+    link: "https://aqt.lbl.gov/"
+  },
+  {
+    name: "Quantum Network Testbed",
+    institution: "LBNL",
     focus: "Quantum Networking",
     funding: "DOE",
-    lat: 41.8419,
-    lng: -88.2419,
-    link: "https://www.fnal.gov/"
+    lat: 37.8753,
+    lng: -122.2570,
+    link: "https://lbl.gov/"
   },
   {
-    name: "MIT Quantum Foundry",
-    institution: "Massachusetts Institute of Technology",
-    focus: "Quantum Materials & Devices",
-    funding: "NSF",
-    lat: 42.3601,
-    lng: -71.0942,
-    link: "https://research.mit.edu/"
-  },
-  {
-    name: "Sandia National Labs",
-    institution: "Sandia",
-    focus: "Quantum Computing & Sensing",
+    name: "QSCOUT",
+    institution: "Sandia National Labs",
+    focus: "Trapped‑ion QC",
     funding: "DOE",
-    lat: 35.0500,
-    lng: -106.5400,
-    link: "https://www.sandia.gov/"
+    lat: 35.1186,
+    lng: -106.6241,
+    link: "https://qscout.sandia.gov/"
   },
   {
-    name: "Chicago Quantum Exchange",
+    name: "Quantum Foundry Facility (Q2F)",
+    institution: "Sandia NL",
+    focus: "Foundry for Quantum Devices",
+    funding: "DOE",
+    lat: 35.1186,
+    lng: -106.6241,
+    link: "https://www.sandia.gov/quantum/"
+  },
+  {
+    name: "Center for Functional Nanomaterials (CFN)",
+    institution: "Brookhaven NL",
+    focus: "Nanomaterials for QIS",
+    funding: "DOE",
+    lat: 40.8690,
+    lng: -72.8740,
+    link: "https://www.bnl.gov/cfn/"
+  },
+  {
+    name: "HQAN",
+    institution: "University of Colorado Boulder",
+    focus: "Quantum Networks",
+    funding: "NSF",
+    lat: 40.0076,
+    lng: -105.2659,
+    link: "https://hqan.colorado.edu/"
+  },
+  {
+    name: "CQE (Center for Quantum Entanglement)",
     institution: "University of Chicago",
     focus: "Quantum Communication",
-    funding: "DOE / NSF",
-    lat: 41.7897,
-    lng: -87.5997,
+    funding: "NSF",
+    lat: 41.7886,
+    lng: -87.5987,
     link: "https://quantum.uchicago.edu/"
   },
   {
-    name: "Oak Ridge National Laboratory",
-    institution: "ORNL",
-    focus: "Quantum Simulation",
-    funding: "DOE",
-    lat: 35.9290,
-    lng: -84.3114,
-    link: "https://www.ornl.gov/"
+    name: "LIGHT-Q",
+    institution: "University of Texas Austin",
+    focus: "Light-Matter QIS",
+    funding: "NSF",
+    lat: 30.2849,
+    lng: -97.7341,
+    link: "https://quantum.utexas.edu/"
+  },
+  {
+    name: "QLCI-FMG",
+    institution: "MIT",
+    focus: "Materials for Quantum Computing",
+    funding: "NSF",
+    lat: 42.3601,
+    lng: -71.0942,
+    link: "https://qis.mit.edu/"
+  },
+  {
+    name: "MIT Lincoln Lab Quantum Network",
+    institution: "MIT Lincoln Laboratory",
+    focus: "Quantum Networking",
+    funding: "DoD/DOE",
+    lat: 42.3616,
+    lng: -71.0920,
+    link: "https://www.ll.mit.edu/"
+  },
+  {
+    name: "Google Quantum AI Campus",
+    institution: "UC Santa Barbara",
+    focus: "Superconducting QC",
+    funding: "Google",
+    lat: 34.4130,
+    lng: -119.8489,
+    link: "https://ai.google/quantum/"
+  },
+  {
+    name: "Station Q",
+    institution: "Microsoft at UC Santa Barbara",
+    focus: "Topological QC",
+    funding: "Microsoft",
+    lat: 34.4134,
+    lng: -119.8450,
+    link: "https://stationq.ucsb.edu/"
+  },
+  {
+    name: "Qubit Incubator",
+    institution: "Elevate Quantum Boulder",
+    focus: "Quantum Startups",
+    funding: "DoC/Federal",
+    lat: 40.0150,
+    lng: -105.2705,
+    link: "https://elevatequantum.org/"
+  },
+  {
+    name: "Quantum New Mexico Institute",
+    institution: "University of New Mexico",
+    focus: "QC for Security",
+    funding: "NSF/DoD",
+    lat: 35.0844,
+    lng: -106.6504,
+    link: "https://quantumnm.org/"
+  },
+  {
+    name: "Quantum Bench Initiative",
+    institution: "Sandia/Los Alamos",
+    focus: "Benchmarking QC",
+    funding: "DoD",
+    lat: 35.8796,
+    lng: -106.3161,
+    link: "https://www.sandia.gov/"
   }
 ];

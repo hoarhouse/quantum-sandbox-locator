4ed5f714a2262f8bd5370f808f493b2cc2753717
hoarhouse <previewcapital@gmail.com>
2025-07-08 11:44:08 -0600

    Create sandbox-data.js

---
+++ sandbox-data.js
@@ -0,0 +1,47 @@
+const sandboxData = [
+  {
+    name: "Fermilab Quantum Network Testbed",
+    institution: "Fermilab",
+    focus: "Quantum Networking",
+    funding: "DOE",
+    lat: 41.8419,
+    lng: -88.2419,
+    link: "https://www.fnal.gov/"
+  },
+  {
+    name: "MIT Quantum Foundry",
+    institution: "Massachusetts Institute of Technology",
+    focus: "Quantum Materials & Devices",
+    funding: "NSF",
+    lat: 42.3601,
+    lng: -71.0942,
+    link: "https://research.mit.edu/"
+  },
+  {
+    name: "Sandia National Labs",
+    institution: "Sandia",
+    focus: "Quantum Computing & Sensing",
+    funding: "DOE",
+    lat: 35.0500,
+    lng: -106.5400,
+    link: "https://www.sandia.gov/"
+  },
+  {
+    name: "Chicago Quantum Exchange",
+    institution: "University of Chicago",
+    focus: "Quantum Communication",
+    funding: "DOE / NSF",
+    lat: 41.7897,
+    lng: -87.5997,
+    link: "https://quantum.uchicago.edu/"
+  },
+  {
+    name: "Oak Ridge National Laboratory",
+    institution: "ORNL",
+    focus: "Quantum Simulation",
+    funding: "DOE",
+    lat: 35.9290,
+    lng: -84.3114,
+    link: "https://www.ornl.gov/"
+  }
+];
